4bab848cfbd4541c42b2f35a4efabe175b26576e
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-08-12 12:11:33 +0700

    Receiving Input

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -7,6 +7,14 @@ price = 10
 # is_published = True
 print(price)
 
-name = 'Kevin'
-age = 20
-is_new = True
+# name = 'Kevin'
+# age = 20
+# is_new = True
+
+# name = input('What is your name? ')
+# print('Hi ' +  name)
+
+name = input('What is your name? ')
+favorite_color = input('What is your favorite color? ')
+
+print(name + ' likes ' + favorite_color)

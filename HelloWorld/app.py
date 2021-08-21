75e81733a685e2e06a2d6912ea0093d3081ffa53
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-08-21 13:43:06 +0700

    comparison operators

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -114,8 +114,27 @@
 
 # Logical Operators
 # has_high_income = False
-has_good_credit = True
-has_criminal_record = True
+# has_good_credit = True
+# has_criminal_record = True
+#
+# if has_good_credit and not has_criminal_record:
+#     print("Eligible for loan")
 
-if has_good_credit and not has_criminal_record:
-    print("Eligible for loan")
+
+
+# Comparison Operators
+# temperature = 35
+#
+# if temperature > 30:
+#     print("It's a hot day")
+# else:
+#     print("It's not a hot day")
+
+name = input('Masukkan nama anda: ')
+
+if len(name) < 3:
+    print("Name must be at least 3 characters.")
+elif len(name) > 50:
+    print("Name must be a maximum of 50 characters.")
+else:
+    print("Name looks good!")

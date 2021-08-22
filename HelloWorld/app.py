054d62d92ef1d43d662749bf7085e9b7d7600ded
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-08-23 00:25:53 +0700

    weight converter program

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -130,11 +130,24 @@
 # else:
 #     print("It's not a hot day")
 
-name = input('Masukkan nama anda: ')
+# name = input('Masukkan nama anda: ')
+#
+# if len(name) < 3:
+#     print("Name must be at least 3 characters.")
+# elif len(name) > 50:
+#     print("Name must be a maximum of 50 characters.")
+# else:
+#     print("Name looks good!")
 
-if len(name) < 3:
-    print("Name must be at least 3 characters.")
-elif len(name) > 50:
-    print("Name must be a maximum of 50 characters.")
+
+
+# Weight Converter Program
+weight = int(input("Weight: "))
+unit = input("(L) bs or (K)g: ")
+
+if unit.upper() == "L":
+    converted = weight * 0.45
+    print(f"You are {converted} kilos")
 else:
-    print("Name looks good!")
+    converted = weight / 0.45
+    print(f"You are {converted} pounds")

667cb7ec4a9504aa10b08310ada0f457d1681956
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-08-30 15:56:44 +0700

    building a guessing game

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -155,8 +155,23 @@
 
 
 # While Loops
-i = 1
-while i <= 5:
-    print('*' * i)
-    i = i + 1
-print("Done")
+# i = 1
+# while i <= 5:
+#     print('*' * i)
+#     i = i + 1
+# print("Done")
+
+
+
+# Building a Guessing Game
+secret_number = 9
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess = int(input('Guess: '))
+    guess_count += 1
+    if guess == secret_number:
+        print('You won!')
+        break
+else:
+    print('Sorry, you failed')

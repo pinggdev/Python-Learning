1e9732f9e3742e6fe11c6e921623c15358794dcd
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-08-30 23:04:32 +0700

    building the car game

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -164,14 +164,44 @@
 
 
 # Building a Guessing Game
-secret_number = 9
-guess_count = 0
-guess_limit = 3
-while guess_count < guess_limit:
-    guess = int(input('Guess: '))
-    guess_count += 1
-    if guess == secret_number:
-        print('You won!')
+# secret_number = 9
+# guess_count = 0
+# guess_limit = 3
+# while guess_count < guess_limit:
+#     guess = int(input('Guess: '))
+#     guess_count += 1
+#     if guess == secret_number:
+#         print('You won!')
+#         break
+# else:
+#     print('Sorry, you failed')
+
+# Building the car game
+command = ""
+started = False
+
+while True:
+    command = input("> ").lower()
+    if command == "start":
+        if started:
+            print("Car is already started!")
+        else:
+            started = True
+            print("Car started...")
+    elif command == "stop":
+        if not started:
+            print("Car is already stopped!")
+        else:
+            started = False
+            print("Car stopped.")
+    elif command == "help":
+        print("""
+start - to start the car
+stop - to stop the car
+quit - to quit
+        """)
+    elif command == "quit":
         break
-else:
-    print('Sorry, you failed')
+    else:
+        print("Sorry, I don't understand that!")
+
